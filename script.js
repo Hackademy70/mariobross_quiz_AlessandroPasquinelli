4536f8cdf1b3316f724ecd6895e00d3e316fe289
Alessandro Pasquinelli <pasquiale89@hotmail.it>
2023-03-11 14:28:17 +0100

    Eserzio MarioBross quiz

---
+++ script.js
@@ -0,0 +1,157 @@
+alert(`Sarai in grado di aiutare Mario a rispondere alle 10 domande e trarre in salvo la principessa?! `);
+alert(`Iniziamo!`);
+alert(`3`);
+alert(`2`);
+alert(`1`);
+let question0= prompt(`Che il gioco inizi! Come viene indicato width in Bootstrap?\n(1) .wh-100 \n(2) .vh-100`);
+
+switch (question0) {
+  case '1':
+    alert(`Indovinato! Puoi passare alla domanda successiva!`);
+    break;
+  case '2':
+    alert(`Risposta errata! Rivediti le lezioni!`); 
+    break;
+  default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;        
+}
+
+let question1 = prompt(`Gioia, Paura, Rabbia, Disgusto e Tristezza guidano le emozioni di chi, in Inside Out? \n(1) Riley \n(2) Andy`);
+
+switch (question1) {
+  case '1':
+    alert(`Risposta esatta! Gli sceneggiatori si sono consultati con psicologi e altri esperti per rendere scientificamente accurato il modo in cui funziona la mente di Riley.`);
+    break;
+  case '2':
+    alert(`Risposta sbagliata! In Toy Story 1 e Toy Story 2, Andy è un bambino di circa 7 anni che passa il suo tempo libero a giocare con i suoi giocattoli, inventando con la sua fantasia storie che li vedono come i buoni che salvano il mondo contrapposti ai cattivi che vogliono conquistarlo.`);
+    break;
+  default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;
+}
+
+let question2 = prompt(`Quanti giocatori ci sono in una squadra di baseball? \n(1) Sette giocatori \n(2) Nove giocatori`);
+
+switch (question2) {
+  case '1':
+    alert(`Risposta errata!`);
+    break;
+  case '2':
+    alert(`Esatto! I nove giocatori sono: lanciatore, ricevitore, prima base, seconda base, interbase, terza base, campo destro, campo centrale e campo sinistro.`);
+    break;
+    default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;        
+}
+let question3 = prompt(`Qual è il diametro di un canestro da basket in centimetri? \n(1)45 cm \n(2)38 cm`);
+
+switch (question3) {
+  case '1':
+    alert(`Risposta Esatta!Tutti i canestri da basket hanno un diametro di 45 cm.`);
+    break;
+  case '2':
+    alert(`Sbaglaito!`);
+    break;
+    default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;        
+}
+let question4 = prompt(`Qual è la capitale dell’Australia?\n(1)Adelaide\(2)Canberra`);
+
+switch (question4) {
+  case '1':
+    alert(`Risposta corretta!È anche chiamata la capitale del Bush, per le sue vaste riserve naturali e le catene montuose circostanti.`);
+    break;
+  case '2':
+    alert(`Sbagliato!Situata tra spiagge di sabbia bianca e dolci colline, Adelaide è una graziosa città con sobri edifici di arenaria, chiese storiche e vasti parchi. È una delle capitali mondiali del vino e ospita alcune delle migliori aziende vinicole australiane.`);
+    break;
+    default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;        
+}
+let question5 = prompt(`Quale paese ha CH per sigla?\n(1)Svizzera\n(2)Cile`);
+
+switch (question5) {
+  case '1':
+    alert(`Risposta esatta!CH sta per le parole latine Confoederatio Helvetica, che significa Confederazione Svizzera.`);
+    break;
+  case '2':
+    alert(`Errato!La sigla del cile è CL`);
+    break;
+    default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;        
+}
+let question6 = prompt(`Chi ha interpretato la regina Elisabetta II nelle prime due stagioni di The Crown?\n(1)Olivia Coleman\n(2)Claire Foy`);
+
+switch (question6) {
+  case '1':
+    alert(`Risposta errata!Ha interpetato nel ruolo della regina nella 3 e 4 stagione di The Crown`);
+    break;
+  case '2':
+    alert(`Esatto!Ha interpretato la regina Elisabetta II negli anni della sua maturità (dai 21 ai 38 anni).`);
+    break;
+    default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;        
+}
+let question7 = prompt(`Quale popolare creatura marina ha la capacità di duplicarsi?\n(1)Polpo\n(2)Medusa`);
+
+switch (question7) {
+  case '1':
+    alert(`Risposta errata!Questo animale si accoppia con delicatezza, allungando un tentacolo “diverso”, chiamato braccio ectocotile, e ponendo, grazie ad esso, una spermatofora (“sacchetto” pieno di liquido seminale) nel corpo della femmina.`);
+    break;
+  case '2':
+    alert(`Esatto!Quando le meduse vengono tagliate a metà, hanno la capacità di rigenerare le proprie cellule e creare altre meduse.`);
+    break;
+    default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;        
+}
+let question8 = prompt(`Come si chiama la femmina del cervo?\n(1)Cerbiatta \n(2)Cerva`);
+
+switch (question8) {
+  case '1':
+    alert(`Risposta errata!Si tratta della femmina del cerbiatto`);
+    break;
+  case '2':
+    alert(`Esatto!Una cerva trascorre la maggior parte dell’anno da sola fino alla nascita dei piccoli, cioè i cerbiatti.`);
+    break;
+    default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;        
+}
+let question9 = prompt(`Quante ossa hanno gli squali?\n(1)zero \(2)quattro`);
+
+switch (question9) {
+  case '1':
+    alert(`Risposta esatta!Lo scheletro degli squali è fatto di cartilagine, non di ossa.`);
+    break;
+  case '2':
+    alert(`Sbaglaito!Alcuni esempi di pesci ossei possono essere il barracuda, pesce zebra e il pesce chirurgo.Essi fanno parte della categoria di pesci ossei o "osteoitti"`);
+    break;
+    default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;        
+}
+let question10 = prompt(` In ambito musicale, cosa significa il termine “piano”?\(1)Da suonare dolcemente\(2)Moderatamente lento`);
+
+switch (question10) {
+  case '1':
+    alert(`Risposta esatta!Si definisce “piano” il livello sonoro quando la musica viene suonata dolcemente.`);
+    break;
+  case '2':
+    alert(`Sbagliato!`);
+    break;
+    default:
+    alert(`Credo che tu non sia in grado di prendere una decisione...`);
+    break;        
+}
+
+alert('Sarai stato in grado di salvare la principessa?')
+if (question0 ==1 && question1==1 && question2 ==2 && question3 ==1 && question4 ==1 && question5 ==1 && question6 ==2 && question7 ==2 && question8 ==2 && question9 ==1 && question10 ==1) {
+    alert(`Complimenti!Sei riuscito a rispondere a tutte le domanda correttamente!Hai salvato la principessa!`)
+} else {
+    alert(`Mi dispiace non ce l'hai fatta!Ritenta la principessa non vede l'ora di essere liberata!`)
+}
